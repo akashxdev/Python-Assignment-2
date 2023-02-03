56855e29c166a29443b442207d75b6b8c3702d32
Akash <123122185+akashxdev@users.noreply.github.com>
2023-02-03 08:51:39 +0530

    2nd assignment

---
+++ 4_Circular_Queue.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularQueue:
+    def __init__(self):
+        self.rear = None
+
+    def is_empty(self):
+        return self.rear == None
+
+    def enqueue(self, data):
+        temp = Node(data)
+        if self.rear == None:
+            self.rear = temp
+            self.rear.next = self.rear
+        else:
+            temp.next = self.rear.next
+            self.rear.next = temp
+            self.rear = temp
+
+    def dequeue(self):
+        if self.rear == None:
+            return None
+        if self.rear.next == self.rear:
+            temp = self.rear
+            self.rear = None
+            return temp.data
+        else:
+            temp = self.rear.next
+            self.rear.next = temp.next
+            return temp.data
+
+    def display(self):
+        if self.rear == None:
+            return None
+        else:
+            temp = self.rear.next
+            while temp != self.rear:
+                print(temp.data, end=' ')
+                temp = temp.next
+            print(temp.data)
+
+
+cq = CircularQueue()
+
+while True:
+    print("Circular Queue using linked list- \n\t1. Enqueue \n\t2. Dequeue \n\t3. Display \n\t4. Quit")
+   
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        data = int(input("Enter the element to be enqueued: "))
+        cq.enqueue(data)
+    elif choice == 2:
+        data = cq.dequeue()
+        if data != None:
+            print("Dequeued element: ", data)
+        else:
+            print("Queue is empty")
+    elif choice == 3:
+        cq.display()
+    elif choice == 4:
+        break
+    else:
+        print("Enter a valid choice.")
